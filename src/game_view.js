c1f6e4cd4e9ebdb21f5fd5249d666ebab5a274fd
Sebastian Sanchez <sebastian@artxcode.io>
2021-04-20 11:30:34 -0400

    getting set up

---
+++ src/game_view.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    game_canvas: document.addEventListener("DOMContentLoaded", () => {
+        const canvasEle = document.getElementById("game-canvas");
+        debugger
+        canvasEle.width = 500;
+        canvasEle.height = 500;
+        console.log("Webpack is working!")
+      
+   })
+};
